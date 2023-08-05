d4033f99cdddf7718a513e64618725ecb56a3782
DanyilUA <122489621+DanyilUA@users.noreply.github.com>
2023-08-05 15:16:01 +0200

    v2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,35 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 
-export class App extends Component {
-  state = {
+export default function FeedBackForm() {
+
+  const [feedbackOptions, setFeedbackOptions] = useState({
     good: 0,
-    neutral: 0,
     bad: 0,
-  };
+    neutral: 0,
+  });
 
-  onLeaveFeedback = option => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
-  };
+  const onLeaveFeedback = option => {
+    setFeedbackOptions(prevState => ({
+      ...prevState, [option]: prevState[option] + 1,
+    }));
+    }
 
-  render() {
-    return (
-      <div
+      return (
+        <div>
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              options={['good', 'neutral', 'bad']}
+              onLeaveFeedback={onLeaveFeedback}
+            />
+          </Section>
 
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
+          <Section title="Statistics">
+            <Statistics options={feedbackOptions} />
+          </Section>
+        </div>
+      );
+}
 
-        <Section title="Statistics">
-          <Statistics options={this.state} />
-        </Section>
-      </div>
-    );
-  }
-};
